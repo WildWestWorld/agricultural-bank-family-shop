c4fbac123fa18e8f272e0b13fdb5c3b571435f47
JOTARO <380686356@qq.com>
2022-09-07 23:18:56 +0800

    完成项目初始化

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import 'amfe-flexible';
+import "@/assets/style/reset.css"
+
+import store from './store/index.js'
+import router from "@/router";
+
+
+
+
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  store,
+  router
 }).$mount('#app')
